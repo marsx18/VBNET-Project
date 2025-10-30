6af79ad83ebb0f1543239a06c359e53b1df4a9fc
mars <ammarsyazani3120@gmail.com>
2025-10-30 16:51:09 +0800

    test database

--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,1 @@
-﻿Public Class Form1
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-End Class
+﻿
