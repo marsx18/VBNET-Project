b7909006178008c210d6cd02651cb7cd2491a8bd
Syimir Aidil <syimiraidilr2@gmail.com>
2025-10-30 22:44:29 +0800

    Test Push 2

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,34 +24,47 @@ Partial Class Form1
     Private Sub InitializeComponent()
         DataGridView1 = New DataGridView()
         Label1 = New Label()
+        Label2 = New Label()
         CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' DataGridView1
         ' 
         DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(201, 133)
+        DataGridView1.Location = New Point(176, 100)
+        DataGridView1.Margin = New Padding(3, 2, 3, 2)
         DataGridView1.Name = "DataGridView1"
         DataGridView1.RowHeadersWidth = 51
-        DataGridView1.Size = New Size(300, 188)
+        DataGridView1.Size = New Size(262, 141)
         DataGridView1.TabIndex = 0
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(274, 95)
+        Label1.Location = New Point(240, 71)
         Label1.Name = "Label1"
-        Label1.Size = New Size(108, 20)
+        Label1.Size = New Size(69, 15)
         Label1.TabIndex = 1
-        Label1.Text = "SYIMIR NIIGGA"
+        Label1.Text = "TEST FORM"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(477, 71)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Label2"
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(700, 338)
+        Controls.Add(Label2)
         Controls.Add(Label1)
         Controls.Add(DataGridView1)
+        Margin = New Padding(3, 2, 3, 2)
         Name = "Form1"
         Text = "Form1"
         CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
@@ -61,5 +74,6 @@ Partial Class Form1
 
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 
 End Class
